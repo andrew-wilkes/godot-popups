d447077a0a8278d6cad60d330863ef9e3ed5bffb
andrew-wilkes <github@urgentclick.com>
2020-05-20 01:48:46 +0900

    Add scenes

---
+++ Main.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _ready():
+	$Button.text = "Open %s popup" % name
---
+++ Menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _ready():
+	$Button.text = "Open %s popup" % name
+
+
+func _on_Button_button_down():
+	pass # Replace with function body.
